ffee1751adfacab87f4c8501301aba326e32c83c
samq-ws <sam.quakenbush@whitesourcesoftware.com>
2022-08-11 22:38:42 -0600

    revert to jq & add checks

--- Repo-Integration/setup.sh
+++ Repo-Integration/setup.sh
@@ -37,16 +37,6 @@ case $SCM in
 
 esac
 
-## Look for Activation Key
-if [ -z "${ws_key}" ]
-then
-    echo "${red}Please set your Activation Key as an environment variable using the following command${end}"
-    echo "${cyn}export ws_key='replace-with-your-activation-key-inside-single-quotes'${end}"
-    exit
-else
-    echo "WS_ACTIVATION_KEY=${ws_key}" > ${MEND_DIR}/${SCM}_settings.env
-fi
-
 ## Dowload agent file and copy to latest
 wget https://integrations.mend.io/release/$AGENT_PATH/$AGENT_TAR -P $BASE_DIR
 AGENT_FILE=$(basename $AGENT_TAR .tar.gz)
@@ -62,6 +52,9 @@ mkdir $BASE_DIR/latest
 mv $BASE_DIR/untar/$AGENT_LATEST* $BASE_DIR/latest
 rm -rf $BASE_DIR/untar
 
+# Copy Activation Key
+jq --arg ws_key $ws_key '(.properties[] | select(.propertyName=="bolt.op.activation.key")).propertyValue |= $ws_key' ${BASE_DIR}/latest/wss-configuration/config/prop.json > ${BASE_DIR}/prop.json
+echo "${grn}${MEND_DIR}/prop.json created successfully${end}"
 
 ## Grab scanner tags
 TAG=$(grep -v ^\# ${BASE_DIR}/latest/build.sh | grep . | awk -F "[ ]" 'NR==1 {print $4}' | awk -F ":" '{print $2}')
@@ -74,23 +67,49 @@ echo "BASE_DIR=${BASE_DIR}" >> ${REPO_INTEGRATION_DIR}/.env
 echo "SCM=$SCM" >> ${REPO_INTEGRATION_DIR}/.env
 
 ## Create Docker Network
-DNETWORK=$(docker network ls -f name=${SCM}_bridge)
+DNETWORK=$(docker network ls -f name=${SCM}_bridge -q)
 if [ -z "$DNETWORK" ]
 then
     echo "${yel}Docker Network does not exist${end}"
     docker network create -d bridge ${SCM}_bridge
     echo "${cyn}Docker Network ${SCM}_bridge created${end}"
 else
-    echo "${cyn}Docker Network ${SCM}_bridge already exists${end}"
+    echo "${cyn}Docker Network ${SCM}_bridge already exists with Network ID ${DNETWORK}${end}"
 fi
 
 echo "${grn}Download Success!!!${end}"
 
 }
 
+function key_check(){
+## Look for Activation Key
+if [ -z "${ws_key}" ]
+then
+    echo "${red}Please set your Activation Key as an environment variable using the following command in order to create ${MEND_DIR}/${SCM}_settings.env${end}"
+    echo "${cyn}export ws_key='replace-with-your-activation-key-inside-single-quotes'${end}"
+    exit
+else
+    scm
+fi
+
+}
+
+function jq_exists(){
+## Check if jq exists on the machine
+JQ_CHECK=$(rpm -qa | grep jq)
+if [ -z "$JQ_CHECK" ]
+then
+    echo "${red}jq could not be found please install with the following command${end}"
+    echo "${cyn}sudo yum install jq${end}"
+    exit
+else
+    key_check
+fi
+}
+
 if [ -z "$1" ]
 then 
     echo "${red}Please pass an scm variable such as gls, bb, or ghe${end}"
 else
-    scm
+    jq_exists
 fi
