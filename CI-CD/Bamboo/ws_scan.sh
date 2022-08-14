eebd618af69a5573362045fd67a1beec867006ed
tabac-ws <ori.tabac@whitesourcesoftware.comgit config --global user.name Ori>
2022-08-14 14:29:21 +0300

    Modify env vars for Bamboo

--- CI-CD/Bamboo/ws_scan.sh
+++ CI-CD/Bamboo/ws_scan.sh
@@ -5,8 +5,13 @@
 # WS_WSS_URL = https://saas.whitesourcesoftware.com/agent
 # WS_APIKEY = {MASKED_APIKEY}
 # WS_USERKEY = {MASKED_USERKEY}
+# Create a Script build step and paste the following:
 
 # Download Unified Agent
+export WS_APIKEY=${bamboo_WS_APIKEY}
+export WS_WSS_URL=${bamboo_WS_WSS_URL}
+export WS_PRODUCTNAME=${bamboo_WS_PRODUCTNAME}
+export WS_PROJECTNAME=${bamboo_WS_PROJECTNAME}
 echo Downloading WhiteSource Unified Agent
 curl -LJO https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar
 if [[ "$(curl -sL https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar.sha256)" != "$(sha256sum wss-unified-agent.jar)" ]] ; then
