77dbfedbd29688e5f0b531bdb3baa9c95322418e
tabac-mend <ori.tabac@whitesourcesoftware.comgit config --global user.name Ori>
2023-03-19 10:14:33 +0200

    Fix API calls

--- Scripts/Mend SCA/check-project-state.sh
+++ Scripts/Mend SCA/check-project-state.sh
@@ -4,17 +4,18 @@
 # WS_GENERATEPROJECTDETAILSJSON: true
 # WS_USERKEY
 # WS_WSS_URL
+# WS_APIKEY
 
 WS_PROJECTTOKEN=$(jq -r '.projects | .[] | .projectToken' ./whitesource/scanProjectDetails.json)
 WS_URL=$(echo $WS_WSS_URL | awk -F "/agent" '{print $1}')
-
+REQUEST_TOKEN=$(curl -s -X POST -H "Content-Type: application/json" -d '{"requestType":"getProjectLastModification", "userKey": "'$WS_USERKEY'", "projectToken":"'$WS_PROJECTTOKEN'"}' $WS_URL/api/v1.3 | jq -r '.projectLastModifications[0].extraData.requestToken')
 IFS="|"
 scan_status=true
-pass_status=("UPDATE"${IFS}"FINISH"${IFS}"DIFF")
-fail_status=("UNKNOWN"${IFS}"FAIL")
+pass_status=("UPDATED"${IFS}"FINISHED")
+fail_status=("UNKNOWN"${IFS}"FAILED")
 while $scan_status
 do
-  new_status=$(curl -s -X POST -H "Content-Type: application/json" -d '{"requestType":"getProjectState", "userKey": "'$WS_USERKEY'", "projectToken":"'$WS_PROJECTTOKEN'"}' $WS_URL/api/v1.3 | jq '.projectState|.lastProcess' | tr -d '"')
+  new_status=$(curl -s -X POST -H "Content-Type: application/json" -d '{"requestType":"getRequestState", "userKey": "'$WS_USERKEY'", "orgToken":"'$WS_APIKEY'", "requestToken":"'$REQUEST_TOKEN'"}' $WS_URL/api/v1.3 | jq -r '.requestState')
   if [[ "${IFS}${pass_status[*]}${IFS}" =~ "${IFS}${new_status}${IFS}" ]];
   then
     scan_status=false
