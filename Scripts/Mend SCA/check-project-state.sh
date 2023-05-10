2513acc13d79f266debe63537f28db4aa931f1be
tabac-mend <ori.tabac@whitesourcesoftware.comgit config --global user.name Ori>
2023-05-10 22:43:19 +0300

    fix check project state

--- Scripts/Mend SCA/check-project-state.sh
+++ Scripts/Mend SCA/check-project-state.sh
@@ -5,29 +5,24 @@
 # WS_USERKEY
 # WS_WSS_URL
 # WS_APIKEY
+# WS_GRADLE_AGGREGATEMODULES or WS_MAVEN_AGGREGATEMODULES - If used Maven and Grade
 
+checkFreq=5
+[[ -z $checkFreq ]] && checkFreq=5
 WS_PROJECTTOKEN=$(jq -r '.projects | .[] | .projectToken' ./whitesource/scanProjectDetails.json)
-WS_URL=$(echo $WS_WSS_URL | awk -F "/agent" '{print $1}')
-REQUEST_TOKEN=$(curl -s -X POST -H "Content-Type: application/json" -d '{"requestType":"getProjectLastModification", "userKey": "'$WS_USERKEY'", "projectToken":"'$WS_PROJECTTOKEN'"}' $WS_URL/api/v1.3 | jq -r '.projectLastModifications[0].extraData.requestToken')
-IFS="|"
-scan_status=true
-pass_status=("UPDATED"${IFS}"FINISHED")
-fail_status=("UNKNOWN"${IFS}"FAILED")
-while $scan_status
-do
-  new_status=$(curl -s -X POST -H "Content-Type: application/json" -d '{"requestType":"getRequestState", "userKey": "'$WS_USERKEY'", "orgToken":"'$WS_APIKEY'", "requestToken":"'$REQUEST_TOKEN'"}' $WS_URL/api/v1.3 | jq -r '.requestState')
-  if [[ "${IFS}${pass_status[*]}${IFS}" =~ "${IFS}${new_status}${IFS}" ]];
-  then
-    scan_status=false
-    echo "Project information has been uploaded successfully!"
-  else
-    echo "Scan is still processing..."
-    sleep 10
-  fi
-  if [[ "${IFS}${fail_status[*]}${IFS}" =~ "${IFS}${new_status}${IFS}" ]];
-  then
-    echo "Scan failed to upload...exiting program"
-    exit 1
-  fi
+WS_API_URL="$(echo "$WS_WSS_URL" | sed 's|agent|api/v1.3|')"
+REQUEST_TOKEN=$(curl -s -X POST -H "Content-Type: application/json" -d '{"requestType":"getProjectLastModification", "userKey": "'$WS_USERKEY'", "projectToken":"'$WS_PROJECTTOKEN'"}' $WS_API_URL | jq -r '.projectLastModifications[0].extraData.requestToken')
+
+ready=false
+while [[ $ready = "false" ]] ; do
+	resProcess="$(curl -s -X POST -H "Content-Type: application/json" -d '{"requestType":"getRequestState", "userKey": "'$WS_USERKEY'", "orgToken":"'$WS_APIKEY'", "requestToken":"'$REQUEST_TOKEN'"}' $WS_API_URL)"
+	repStatus="$(echo "$resProcess" | jq -r '.requestState')"
+
+  if [[ $repStatus = "FINISHED" || $repStatus = "UPDATED" ]] ; then
+		ready=true
+		echo "Project information has been uploaded successfullly\!"
+  elif [[ $repStatus = "IN_PROGRESS" ]] ; then
+		echo "Scan is still processing..."
+    sleep $checkFreq
+	fi
 done
-unset IFS
