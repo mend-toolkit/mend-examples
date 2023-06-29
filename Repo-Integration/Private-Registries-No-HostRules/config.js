51964563f04132c17a2bf70adf26b03c131f7735
Kyal Lanum <83832885+kyallanum-MND@users.noreply.github.com>
2023-06-29 16:25:24 -0600

    Updating what I have

--- Repo-Integration/Private-Registries-No-HostRules/config.js
+++ Repo-Integration/Private-Registries-No-HostRules/config.js
@@ -1,14 +1,24 @@
 module.exports = {
 	"packageRules": [{
 		"matchManagers": ["maven"],
-		"registryUrls": ["https://<artifactory_instance>.jfrog.io/artifactory/libs-release", "https://<artifactory_instance>.jfrog.io/artifactory/libs-snapshot"]
+		"registryUrls": [process.env.MVN_RELEASE, process.env.MVN_SNAPSHOT]
+	},
+	{
+		"matchManagers": ["npm"],
+		"registryUrls": [ process.env.NPM_REGISTRY ]
 	}],
 	"hostRules": [
 		{
 			"hostType": "maven",
-			"matchHost": "https://<artifactory_instance>.jfrog.io/artifactory",
-			"username": "<artifactory_username>",
-			"password": "<artifactory_password_not_encrypted>"
+			"matchHost": process.env.MVN_BASE_URL,
+			"username": process.env.MAVEN_USER,
+			"password": process.env.MAVEN_PASS
+		},
+		{
+			"hostType": "npm",
+			"matchHost": process.env.NPM_REGISTRY,
+			"username": process.env.NPM_EMAIL,
+			"password": process.env.NPM_PASS
 		}
 	]
 }
