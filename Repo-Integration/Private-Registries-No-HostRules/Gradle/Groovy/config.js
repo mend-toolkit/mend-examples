24b30acca7b039ba9e82eb095c65d0f85800c451
Kyal Lanum <kyal.lanum@whitesourcesoftware.com>
2023-07-26 14:38:15 -0600

    Fixed some configuration in config.js/docker-compose.yml
    Signed-off-by: Kyal Lanum <kyal.lanum@whitesourcesoftware.com>

--- Repo-Integration/Private-Registries-No-HostRules/Gradle/Groovy/config.js
+++ Repo-Integration/Private-Registries-No-HostRules/Gradle/Groovy/config.js
@@ -1,12 +1,12 @@
 module.exports = {
 	"packageRules": [{
 		"matchManagers": [ "gradle", "gradle-wrapper" ],
-		"registryUrls": [ process.env.MAVEN_REGISTRY, process.env.GRADLE_PLUGIN_REGISTRY ]
+		"registryUrls": [ process.env.MVN_RELEASE, process.env.GRADLE_PLUGIN_REGISTRY ]
 	}],
 	"hostRules" : [
 		{
 			"hostType": "maven",
-			"matchHost": process.env.MVN_BASE_URL,
+			"matchHost": process.env.MVN_RELEASE,
 			"username": process.env.MVN_USER,
 			"password": process.env.MVN_PASS
 		},
