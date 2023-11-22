0b5b0d9efd84ecb9dae04401ca72992e0ae52167
Kyal Lanum <kyal.lanum@whitesourcesoftware.com>
2023-11-22 13:03:59 -0700

    Added more comments/documentation to the script for better understanding.
    Signed-off-by: Kyal Lanum <kyal.lanum@whitesourcesoftware.com>

--- Scripts/Mend SCA/get-malicious-packages.sh
+++ Scripts/Mend SCA/get-malicious-packages.sh
@@ -1,30 +1,56 @@
 #!/bin/bash
+#
+# ******** Mend Script to Pull Malicious Package Alerts for Notifications ********
+# 
+# Users should edit this file to add any steps for consuming the information provided by the API requests however needed.
+# 
+# For more information on the APIs used, please check our REST API documentation page:
+# 📚 https://docs.mend.io/bundle/mend-api-2-0/page/index.html
+#
+# ******** Description ********
+# This script pulls all of the products in an organization and then retrieves the Malicious Packages for each.
+# Mend marks any malicious packages with a Vulnerability ID starting with: "MSC".
+# A good way to get this information is to return all alerts for a product with the search filter: "search=vulnerabilityName:LIKE:MSC"
+# Afterwards the scripts combines all of the data pulled for each product.
+# 
+# The WS_API_KEY environment variable is optional. If this is not specified in the script, then the Login API will
+# authenticate to the last organization the user accessed in the Mend UI.
+
 # Prerequisites:
 # apt install jq curl
 # MEND_USER_KEY - An administrator's userkey
 # MEND_EMAIL - The administrator's email
 # WS_APIKEY - API Key for organization (optional)
-# MEND_URL - e.g. https://api-saas.mend.io/api/v2.0
+# MEND_URL - e.g. https://saas.mend.io/
+
+# Reformat MEND_URL for the API to https://api-<env>/api/v2.0
+MEND_API_URL=$(echo "${MEND_URL}" | sed -E 's/(saas|app)(.*)/api-\1\2\/api\/v2.0/g')
 
 # If API Key was not specified then exclude from Login request body.
 if [ -z "$WS_APIKEY" ]
 then
+	echo "WS_APIKEY environment variable was not provided."
+       	echo -e "The Login API will default to the last organization this user accessed in the Mend UI.\n"
         LOGIN_BODY="{\"email\": \"$MEND_EMAIL\", \"userKey\": \"$MEND_USER_KEY\" }"
 else
+	echo -e "Logging in with provided API key.\n"
         LOGIN_BODY="{\"email\": \"$MEND_EMAIL\", \"userKey\": \"$MEND_USER_KEY\", \"orgToken\": \"$WS_APIKEY\"}"
 fi
 
 # Log into API 2.0 and get the JWT Token and Organization UUID
-LOGIN_RESPONSE=$(curl -s -X POST --location "$MEND_URL/login" --header 'Content-Type: application/json' --data-raw $LOGIN_BODY)
+LOGIN_RESPONSE=$(curl -s -X POST --location "$MEND_API_URL/login" --header 'Content-Type: application/json' --data-raw "${LOGIN_BODY}")
 
 JWT_TOKEN=$(echo $LOGIN_RESPONSE | jq -r '.retVal.jwtToken')
 WS_APIKEY=$(echo $LOGIN_RESPONSE | jq -r '.retVal.orgUuid')
 
 # Get all products
-PRODUCT_API_RESPONSE=$(curl -s --location "$MEND_URL/orgs/$WS_APIKEY/products?pageSize=10000&page=0" --header 'Content-Type: application/json' --header "Authorization: Bearer $JWT_TOKEN")
+echo "Retrieving Products from Organization"
+PRODUCT_API_RESPONSE=$(curl -s --location "$MEND_API_URL/orgs/$WS_APIKEY/products?pageSize=10000&page=0" --header 'Content-Type: application/json' --header "Authorization: Bearer $JWT_TOKEN")
 NUM_PRODUCTS=$(echo $PRODUCT_API_RESPONSE | jq -r '.additionalData.totalItems' )
 PRODUCTS=$(echo $PRODUCT_API_RESPONSE | jq '.retVal')
 
+
+# This output variable starts with nothing, and will get populated with data as we pull the alerts for each product.
 OUTPUT="[]"
 
 # Loop through each product in $PRODUCTS and get the uuid associated with it.
@@ -34,12 +60,29 @@ do
         echo "Getting Malicious Packages for Product $(($i+1))/$NUM_PRODUCTS: $CURRENT_PRODUCT_TOKEN"
 
         # Get all security alerts that have "MSC" in the name
-        CURRENT_OUTPUT=$(curl -s --location "$MEND_URL/products/$CURRENT_PRODUCT_TOKEN/alerts/security?pageSize=10000&page=0&search=vulnerabilityName%3ALIKE%3AMSC" --header 'Content-Type: application/json' --header "Authorization: Bearer $JWT_TOKEN" | jq '.retVal')
+        CURRENT_OUTPUT=$(curl -s --location "$MEND_API_URL/products/$CURRENT_PRODUCT_TOKEN/alerts/security?pageSize=10000&page=0&search=vulnerabilityName%3ALIKE%3AMSC" --header 'Content-Type: application/json' --header "Authorization: Bearer $JWT_TOKEN" | jq '.retVal')
 
-        # Add the current value to $OUTPUT
+        # This takes the current output and merges it with what is already in $OUTPUT, allowing for one JSON object to get returned for easy parsing.
         OUTPUT=$(echo "$OUTPUT $CURRENT_OUTPUT" | jq -s add)
 done
 
 # Extract all the information we need for an alert
 OUTPUT=$(echo $OUTPUT | jq '[.[] | {productName: .product.name, projectName: .project.name, vulnId: .name, libraryName: .component.name, vulnScore: .vulnerability.vulnerabilityScoring }]')
 echo -e "\nMalicious Packages: \n$OUTPUT"
+
+# $OUTPUT can now be used in any notification process such as an email, consume it in another alerting system.
+# The format of $OUTPUT will be:
+# [
+# 	{
+# 		"productName": "productX",
+#		"projectName": "projectX",
+#		"vulnId": "MSC-xxxx-yyyyy",
+#		"libraryName": "libraryX",
+#		"vulnScore": {
+#			...
+#		}
+# 	},
+#	{
+#		...
+#	}
+# ]
