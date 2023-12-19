e1a9548d7c3dff1839a691b4598e9a038af8c024
Sam Quakenbush <sam.quakenbush@whitesourcesoftware.com>
2023-12-19 10:40:08 -0700

    update self-managed disclaimer

--- CI-CD/Jenkins/Mend CLI/Jenkins.groovy
+++ CI-CD/Jenkins/Mend CLI/Jenkins.groovy
@@ -103,7 +103,8 @@ pipeline {
                 sh '''
                 export repo=$(basename -s .git $(git config --get remote.origin.url))
                 export branch=$(git rev-parse --abbrev-ref HEAD)
-                ./mend code --non-interactive
+                ./mend code --non-interactive -s *//${JOB_NAME}//${repo}_${branch}
+                '''
                 }
             }
         }
