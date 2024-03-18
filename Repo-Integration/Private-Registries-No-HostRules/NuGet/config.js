37614785adda700924cd426f90a71e3854f810f4
Kyal Lanum <kyal.lanum@mend.io>
2024-03-18 11:49:31 -0600

    Adds required NuGet/config.js; Adds more information/formatting to README.md

---
+++ Repo-Integration/Private-Registries-No-HostRules/NuGet/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "packageRules": [{
+    "matchManagers": [ "nuget" ],
+    "registryUrls":  [ process.env.NUGET_REGISTRY ]
+  }],
+  "hostRules": [{
+    "hostType": "nuget",
+    "matchHost": process.env.NUGET_REGISTRY,
+    "userName": process.env.NUGET_USER,
+    "password": process.env.NUGET_PASS
+  }]
+}
