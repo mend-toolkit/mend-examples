0637a487a54eadf9ea2001e548c64e8b1618fa67
tabac-mend <ori.tabac@whitesourcesoftware.comgit config --global user.name Ori>
2024-06-23 21:56:41 +0300

    Add docs to advanced pipelines

--- CI-CD/Jenkins/Mend CLI/vars/MendSCAScan.groovy
+++ CI-CD/Jenkins/Mend CLI/vars/MendSCAScan.groovy
@@ -4,8 +4,14 @@ def call() {
             sh '''
             export repo=$(basename -s .git $(git config --get remote.origin.url))
             export branch=$(git rev-parse --abbrev-ref HEAD)
-            ./mend dep -u -s *//${JOB_NAME}//${repo}_${branch} --fail-policy --non-interactive --export-results dep-results.txt
+            ./mend dep -u -s "*//${JOB_NAME}//${repo}_${branch} --fail-policy" --non-interactive --export-results dep-results.txt
+            if [[ "$dep_exit" == "9" ]]; then
+                  echo "[warning]  Dependency scan policy violation"
+            else
+                  echo "No policy violations found in dependencies scan"
+            fi          
             '''
-            archiveArtifacts artifacts: "dep-results.txt", fingerprint: true
       }
+      archiveArtifacts artifacts: "dep-results.txt", fingerprint: true
+
 }
