993dea1952dd0fba53511903a2b63b07e82f3f60
JRMendDemo <122036228+JRMendDemo@users.noreply.github.com>
2024-06-24 10:29:47 -0500

    Update MendSCAScan.groovy

--- CI-CD/Jenkins/Mend CLI/vars/MendSCAScan.groovy
+++ CI-CD/Jenkins/Mend CLI/vars/MendSCAScan.groovy
@@ -4,7 +4,7 @@ def call() {
             sh '''
             export repo=$(basename -s .git $(git config --get remote.origin.url))
             export branch=$(git rev-parse --abbrev-ref HEAD)
-            ./mend dep -u -s "*//${JOB_NAME}//${repo}_${branch} --fail-policy" --non-interactive --export-results dep-results.txt
+            ./mend dep -u -s "*//${JOB_NAME}//${repo}_${branch}" --fail-policy --non-interactive --export-results dep-results.txt
             if [[ "$dep_exit" == "9" ]]; then
                   echo "[warning]  Dependency scan policy violation"
             else
