577d863f44314dd4289d4ee623d9cdf9c40eab06
tabac-mend <ori.tabac@whitesourcesoftware.comgit config --global user.name Ori>
2024-11-06 14:21:41 +0200

    Update jenkins SCA groovy

--- CI-CD/Jenkins/Mend CLI/vars/MendSCAScan.groovy
+++ CI-CD/Jenkins/Mend CLI/vars/MendSCAScan.groovy
@@ -1,32 +1,23 @@
 def call(Map args = [:]) { 
     boolean reachability = args.get('reachability', false)
     echo 'Run Mend dependencies scan'
+
     catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-        if (reachability) {
-            sh """
-            export repo=\$(basename -s .git \$(git config --get remote.origin.url))
-            export branch=\$(git rev-parse --abbrev-ref HEAD)
-            ./mend dep -u -r -s "*//\${JOB_NAME}//\${repo}_\${branch}" --fail-policy --non-interactive --export-results dep-results.txt
-            dep_exit=\$?
-            if [[ "\$dep_exit" == "9" ]]; then
-                echo "[warning]  Dependency scan policy violation"
-            else
-                echo "No policy violations found in dependencies scan"
-            fi
-            """
-        } else {
-            sh """
-            export repo=\$(basename -s .git \$(git config --get remote.origin.url))
-            export branch=\$(git rev-parse --abbrev-ref HEAD)
-            ./mend dep -u -s "*//\${JOB_NAME}//\${repo}_\${branch}" --fail-policy --non-interactive --export-results dep-results.txt
-            dep_exit=\$?
-            if [[ "\$dep_exit" == "9" ]]; then
-                echo "[warning]  Dependency scan policy violation"
-            else
-                echo "No policy violations found in dependencies scan"
-            fi
-            """
-        }
+        def reachabilityFlag = reachability ? "-r" : ""
+
+        sh """
+        export repo=\$(basename -s .git \$(git config --get remote.origin.url))
+        export branch=\$(git rev-parse --abbrev-ref HEAD)
+
+        ./mend dep -u ${reachabilityFlag} -s "*//\${JOB_NAME}//\${repo}_\${branch}" --fail-policy --non-interactive --export-results dep-results.txt
+        
+        dep_exit=\$?
+        if [[ "\$dep_exit" == "9" ]]; then
+            echo "[warning] Dependency scan policy violation"
+        else
+            echo "No policy violations found in dependencies scan"
+        fi
+        """
     }
     archiveArtifacts artifacts: "dep-results.txt", fingerprint: true
 }
