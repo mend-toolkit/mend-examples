1c77b3d5082dc894403b217f0e2a9960464fa6f3
Hugh Cushing <hugh.cushing@mend.io>
2025-01-29 14:49:16 -0500

    fix2

--- Repo-Integration/Self-Managed/setup.sh
+++ Repo-Integration/Self-Managed/setup.sh
@@ -91,7 +91,7 @@ function get_commandline_flags(){
 
 function env_port_check() {
     ## Check if operating system is Ubuntu
-    lsb_release 2>&1 >/dev/null
+    lsb_release &> /dev/null
 
     if [[ $? -ne 0 ]] || [[ $(lsb_release -is | grep Ubuntu) -ne "Ubuntu" ]]; then
         if [[ $(uname | grep -o ^MINGW64_NT) == "MINGW64_NT" ]]; then
