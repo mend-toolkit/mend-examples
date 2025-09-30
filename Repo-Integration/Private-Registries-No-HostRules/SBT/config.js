9cf9f4b0701a80a95640ea838b0b3a646c2e19f3
kirilurdemocorp-mend <kiril.urbonas@mend.io>
2025-09-30 18:47:28 +0300

    Update config.js

--- Repo-Integration/Private-Registries-No-HostRules/SBT/config.js
+++ Repo-Integration/Private-Registries-No-HostRules/SBT/config.js
@@ -1,36 +1,24 @@
 module.exports = {
-  // Auth for your Artifactory
+  packageRules: [
+    {
+      // Main Maven registry for sbt dependencies
+      matchManagers: ["sbt"],
+      matchDatasources: ["maven"],
+      registryUrls: ["https://<artifactory>.jfrog.io/artifactory/libs-release"]
+    },
+    {
+      // SBT plugins 
+      matchManagers: ["sbt"],
+      matchDatasources: ["maven"],
+      registryUrls: ["https://<artifactory>.jfrog.io/artifactory/sbt-plugins"]
+    },
+  ],
   hostRules: [
     {
       hostType: "maven",
-      matchHost: process.env.RENOVATE_HOST,     // e.g., rivernetm.jfrog.io
-      username: process.env.RENOVATE_USER,
-      password: process.env.RENOVATE_PASS
-    },
-    {
-      hostType: "ivy",
-      matchHost: process.env.RENOVATE_HOST,
-      username: process.env.RENOVATE_USER,
-      password: process.env.RENOVATE_PASS
+      matchHost: "<artifactory>.jfrog.io",
+      username: process.env.SBT_USER || "",
+      password: process.env.SBT_PASS || ""
     }
-  ],
-
-  // Force managers to use your virtual repos (not public)
-  packageRules: [
-    {
-      matchManagers: ["maven", "gradle"],
-      registryUrls: [process.env.RENOVATE_MAVEN_REGISTRY] // e.g. https://<host>/artifactory/libs-release
-    },
-    {
-      matchManagers: ["sbt"],
-      registryUrls: [
-        process.env.RENOVATE_MAVEN_REGISTRY, // many sbt deps are on Maven
-        process.env.RENOVATE_IVY_REGISTRY    // sbt plugin releases (Ivy)
-      ]
-    }
-  ],
-
-  // (Optional) keep PRs manageable
-  prConcurrentLimit: 5,
-  prHourlyLimit: 3
+  ]
 };
